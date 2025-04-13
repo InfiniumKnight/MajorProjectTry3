ddf972b1230b85896cb23f8161baef9194789bcf
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2025-04-13 00:02:12 -0400

    Landscape Collision

--- Assets/Scripts/LandscapeGenerator.cs
+++ Assets/Scripts/LandscapeGenerator.cs
@@ -11,11 +11,12 @@ public class LandscapeGenerator : MonoBehaviour
     UnityEngine.Vector3[] vertices;
     int[] triangles;
     UnityEngine.Vector2[] uvs;
-    Mesh mesh;
+    Mesh LandscapeMesh;
     public int xSize;
     public int zSize;
     float xOffset;
     float zOffset;
+    MeshCollider LandscapeCollision;
 
     // Start is called before the first frame update
     void Start()
@@ -23,33 +24,34 @@ public class LandscapeGenerator : MonoBehaviour
         xOffset = UnityEngine.Random.Range(0f,9999f);
         zOffset = UnityEngine.Random.Range(0f,9999f);
 
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+        LandscapeMesh = new Mesh();
+        LandscapeMesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        GetComponent<MeshFilter>().mesh = LandscapeMesh;
 
         CreateLandscape();
         UpdateMesh();
     }
-
+    
     void CreateLandscape()
     {
-       vertices = new UnityEngine.Vector3[(xSize + 1) * (zSize + 1)];
+        vertices = new UnityEngine.Vector3[(xSize + 1) * (zSize + 1)];
 
-       for (int i = 0, z = 0; z <= zSize; z++)
-       {
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
             for (int x = 0; x <= xSize; x++)
             {
                 float y = Mathf.PerlinNoise((xOffset + x) * 0.1f, (zOffset + z) * 0.1f) * 3f;
-                vertices[i] = new UnityEngine.Vector3(x,y,z);
+                vertices[i] = new UnityEngine.Vector3(x- xSize / 2f ,y,z - zSize / 2f);
                 i++;
             }
-       }
+        }
 
-       triangles = new int[xSize * zSize * 6];
-       int vert = 0;
-       int tris = 0;
+        triangles = new int[xSize * zSize * 6];
+        int vert = 0;
+        int tris = 0;
 
-       for (int z = 0; z < zSize; z++)
-       {
+        for (int z = 0; z < zSize; z++)
+        {
             for (int x = 0; x < xSize; x++)
             {
                 triangles[tris + 0] = vert + 0;
@@ -63,25 +65,32 @@ public class LandscapeGenerator : MonoBehaviour
                 tris += 6;
             }
             vert++;
-       }
+        }
 
-       uvs = new UnityEngine.Vector2[vertices.Length];
-       for (int i = 0, z = 0; z <= zSize; z++)
-       {
+        uvs = new UnityEngine.Vector2[vertices.Length];
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
             for (int x = 0; x <= xSize; x++)
             {
                 uvs[i] = new UnityEngine.Vector2((float)x, (float)z);
                 i++;
             }
-       }
+        }
     }
 
     void UpdateMesh()
     {
-        mesh.Clear();
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.RecalculateNormals();
-        mesh.uv = uvs;
+        LandscapeMesh.Clear();
+        LandscapeMesh.vertices = vertices;
+        LandscapeMesh.triangles = triangles;
+        LandscapeMesh.RecalculateNormals();
+        LandscapeMesh.uv = uvs;
+
+        LandscapeCollision = gameObject.GetComponent<MeshCollider>();
+        if(LandscapeCollision == null)
+            LandscapeCollision = gameObject.AddComponent<MeshCollider>();
+        LandscapeCollision.sharedMesh = LandscapeMesh;
+
+        
     }
 }
