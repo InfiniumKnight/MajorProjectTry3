f272dc0bcc9799e57603d551216a8aeef9fc3d6e
aknez615 <al683193@ucf.edu>
2025-04-26 19:13:41 -0400

    glitch script

---
+++ Assets/Scripts/UI/GlitchButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GlitchButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        animator.SetTrigger("Glitch");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //Idle
+    }
+}
